a03e446522f83dba05ba33ad93911b088d18712c
unknown <pharellc.dev@gmail.com>
2025-09-24 13:48:09 +0200

    job06

--- jour 7/job 06/index.php
+++ jour 7/job 06/index.php
@@ -1,14 +1,29 @@
 <?php 
 
-function leetSpeak($str){
-    if ($str == "A"||"a"){
-        return 4;
-}   else if ($str == "B"||"b"){
-        return 8;
-}   else if ($str == "E"||"e"){
-        return 3;
-}   else if($str == "G"||"g"){
-        return 6;
-}};
 
-echo leetSpeak("a");
+
+function leetSpeak($str) {
+    if ($str == "A" || $str == "a") {
+        return "4";
+    } else if ($str == "B" || $str == "b") {
+        return "8";
+    } else if ($str == "E" || $str == "e") {
+        return "3";
+    } else if ($str == "G" || $str == "g") {
+        return "6";
+    } else if ($str == "L" || $str == "l") {
+        return "1";
+    } else if ($str == "I" || $str == "i") {
+        return "1";
+    } else if ($str == "O" || $str == "o") {
+        return "0";
+    } else if ($str == "S" || $str == "s") {
+        return "5";
+    } else if ($str == "T" || $str == "t") {
+        return "7";
+    } else {
+        return $str;
+    }
+}
+
+echo leetSpeak("E");
